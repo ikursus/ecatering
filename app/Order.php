e616a9c1365428cf3346e38db5a53742fc707f39
jomphp <gwbiz4u@gmail.com>
2017-03-02 15:57:42 +0800

    Update $fillable

--- app/Order.php
+++ app/Order.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    // Mass Assigments
+    protected $fillable = [
+      'product_id',
+      'customer_name',
+      'customer_email',
+      'customer_phone',
+      'customer_address',
+      'status',
+      'payment_amount'
+    ];
 }
